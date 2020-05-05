f3a4bfa30bbca415ff1d78c095ec48413fa8326b
Marcus Corbin <mgc1g18@soton.ac.uk>
2020-05-05 17:24:45 +0100

    drawing to pixmap and emitting signal when a new image is drawn

--- drawarea.cpp
+++ drawarea.cpp
@@ -1,6 +1,55 @@
+#include <QMouseEvent>
+#include <QPainter>
+
 #include "drawarea.h"
 
-DrawArea::DrawArea(QWidget *parent) : QWidget(parent)
+DrawArea::DrawArea(QWidget *parent)
+    : QWidget(parent), m_PenColour(Qt::black), m_Pixmap(800, 600)
+{
+    m_Pixmap.fill(Qt::white);
+}
+
+DrawArea::~DrawArea()
 {
 
+}
+
+void DrawArea::clearDrawArea()
+{
+    m_Pixmap.fill(Qt::white);
+    update();
+}
+
+void DrawArea::setPenColour(const QColor &c)
+{
+    m_PenColour = c;
+}
+
+
+void DrawArea::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+
+    QPainter painter(this);                 // instanitate a QPainter that will draw to the current widget
+    painter.drawPixmap(0, 0, m_Pixmap);     // draw the saved pixmap
+}
+
+void DrawArea::mousePressEvent(QMouseEvent *event)
+{
+    m_OldPoint = event->pos();
+}
+
+void DrawArea::mouseMoveEvent(QMouseEvent *event)
+{
+    QPainter painter(&m_Pixmap);                    // instatiate a QPaitner that will draw to the Pixmap
+    painter.setPen(QPen(m_PenColour, 3));           // set what colour the pen is based on m_PenColour
+    painter.drawLine(m_OldPoint, event->pos());     // draw a line between the old mouse position and the new one
+
+    QRect rect(m_OldPoint, event->pos());
+    rect = rect.normalized();
+    update(rect.x() - 1, rect.y() - 1, rect.width() + 2, rect.height() + 2);        // only update the area that has been drawn to to save us re drawing the entire pixmap
+
+    emit pixmapUpdated(m_Pixmap);
+
+    m_OldPoint = event->pos();
 }
--- drawarea.h
+++ drawarea.h
@@ -8,8 +8,26 @@ class DrawArea : public QWidget
     Q_OBJECT
 public:
     explicit DrawArea(QWidget *parent = nullptr);
+    ~DrawArea();
+
+    QColor penColour() const { return m_PenColour; }
+
+public slots:
+    void clearDrawArea();
+    void setPenColour(const QColor &c);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
 
 signals:
+    void pixmapUpdated(QPixmap &p);
+
+private:
+    QColor m_PenColour;
+    QPixmap m_Pixmap;       //&& will probably have to change this for something wil less overhead...
+    QPoint m_OldPoint;
 
 };
 
